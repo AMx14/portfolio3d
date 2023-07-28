1f52d70293509edfccc5572cd28b3e6ad96c60a0
xdozh <140817727+xdozh@users.noreply.github.com>
2023-07-28 17:30:43 +0400

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -78,6 +78,18 @@ const Contact = () => {
     <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
       <motion.div variants={slideIn("left", "tween", 0.2, 1)} className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
         <p className={styles.sectionSubText}>Get in touch</p>
+          <div>
+              {/* Add your social handles here with icons */}
+              <a href="https://www.linkedin.com/your-profile" target="_blank" rel="noopener noreferrer" className='text-white font-medium flex items-center social-link'>
+                  <FontAwesomeIcon icon={faLinkedin} className="mr-2" />
+              </a>
+              <a href="https://leetcode.com/your-profile" target="_blank" rel="noopener noreferrer" className='text-white font-medium flex items-center'>
+                  <img src={leetcodeIcon} alt="LeetCode" className="w-6 h-6 mr-2" />
+              </a>
+              <a href="https://codeforces.com/your-profile" target="_blank" rel="noopener noreferrer" className='text-white font-medium flex items-center'>
+                  <img src={codeforcesIcon} alt="CodeForces" className="w-6 h-6 mr-2" />
+              </a>
+          </div>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
 
         
@@ -90,7 +102,7 @@ const Contact = () => {
               name='name'
               value={form.name}
               onChange={handleChange}
-              placeholder="What's your good name?"
+              placeholder="What's your name?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
@@ -101,7 +113,7 @@ const Contact = () => {
               name='email'
               value={form.email}
               onChange={handleChange}
-              placeholder="What's your web address?"
+              placeholder="Your email address?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
@@ -112,7 +124,7 @@ const Contact = () => {
               name='message'
               value={form.message}
               onChange={handleChange}
-              placeholder='What you want to say?'
+              placeholder='Your message'
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
@@ -121,18 +133,6 @@ const Contact = () => {
             {loading ? "Sending..." : "Send"}
           </button>
         </form>
-        <div className="social-icons-container">
-          {/* Add your social handles here with icons */}
-          <a href="https://www.linkedin.com/your-profile" target="_blank" rel="noopener noreferrer" className='text-white font-medium flex items-center social-link'>
-            <FontAwesomeIcon icon={faLinkedin} className="mr-2" />
-          </a>
-          <a href="https://leetcode.com/your-profile" target="_blank" rel="noopener noreferrer" className='text-white font-medium flex items-center'>
-              <img src={leetcodeIcon} alt="LeetCode" className="w-6 h-6 mr-2" />
-          </a>
-          <a href="https://codeforces.com/your-profile" target="_blank" rel="noopener noreferrer" className='text-white font-medium flex items-center'>
-              <img src={codeforcesIcon} alt="CodeForces" className="w-6 h-6 mr-2" />
-          </a>            
-        </div>
         
       </motion.div>
       
